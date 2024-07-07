f9096110e563f81c51fc2b7bebfd561c0ddfc184
sujaybn <89024189+sujaybn@users.noreply.github.com>
2024-07-07 23:07:02 +0530

    feat: initialize StringCalculator with basic add method and initial tests
    - Added StringCalculator class with a basic add method
    - Implemented initial test cases for empty string, single number, and two numbers

---
+++ src/main/java/org/calc/assesment/StringCalculator.java
@@ -0,0 +1,10 @@
+package org.calc.assesment;
+
+public class StringCalculator {
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/org/calc/assesment/StringCalculatorTest.java
@@ -0,0 +1,27 @@
+package org.calc.assesment;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+    @Test
+    public void testEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void testSingleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    public void testTwoNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("1,2"));
+    }
+}
