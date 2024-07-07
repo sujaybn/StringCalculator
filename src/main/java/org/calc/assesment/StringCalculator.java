15e8a48ba541956bf27a77b0aab55c18ab08f08e
sujaybn <89024189+sujaybn@users.noreply.github.com>
2024-07-07 23:20:53 +0530

    Added implementation to take newline charecter in between Strings to perform addition

--- src/main/java/org/calc/assesment/StringCalculator.java
+++ src/main/java/org/calc/assesment/StringCalculator.java
@@ -6,7 +6,7 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] numArray = numbers.split(",");
+        String[] numArray = numbers.split("[,\n]");
         int sum = 0;
         for (String num : numArray) {
             sum += Integer.parseInt(num);
