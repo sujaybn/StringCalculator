e70bfd034acabed9f03d4fe6b6ba48547d62a935
sujaybn <89024189+sujaybn@users.noreply.github.com>
2024-07-08 09:02:21 +0530

    Adding test case to check the addition of negative values

--- src/test/java/org/calc/assesment/StringCalculatorTest.java
+++ src/test/java/org/calc/assesment/StringCalculatorTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
@@ -24,10 +26,18 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(3, calculator.add("1,2"));
     }
+
     @Test
     public void testNewLinesBetweenNumbers() {
         StringCalculator calculator = new StringCalculator();
         assertEquals(6, calculator.add("1\n2,3"));
     }
-
+    @Test
+    public void testNegativeNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculator.add("1,-2,3");
+        });
+        Assertions.assertEquals("Negative numbers not allowed: [-2]", thrown.getMessage());
+    }
 }
