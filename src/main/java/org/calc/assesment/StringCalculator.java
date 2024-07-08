9cfed1189c3b040c8f89f01f258a937a51f60eca
sujaybn <89024189+sujaybn@users.noreply.github.com>
2024-07-08 09:06:32 +0530

    Adding implementation for addition of values with delimiters and negative numbers

--- src/main/java/org/calc/assesment/StringCalculator.java
+++ src/main/java/org/calc/assesment/StringCalculator.java
@@ -1,16 +1,37 @@
 package org.calc.assesment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        String[] numArray = numbers.split("[,\n]");
-        int sum = 0;
-        for (String num : numArray) {
-            sum += Integer.parseInt(num);
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
         }
+
+        String[] numArray = numbers.split(delimiter);
+        int sum = 0;
+        List<Integer> negativeNumbers = new ArrayList<>();
+
+        for (String num : numArray) {
+            int number = Integer.parseInt(num);
+            if (number < 0) {
+                negativeNumbers.add(number);
+            }
+            sum += number;
+        }
+
+        if (!negativeNumbers.isEmpty()) {
+            throw new IllegalArgumentException("Negative numbers not allowed: " + negativeNumbers.toString());
+        }
+
         return sum;
     }
 }
