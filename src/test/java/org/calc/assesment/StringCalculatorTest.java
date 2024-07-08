3cb4b06dfe5cbd337ce61b905872f2986281285d
sujaybn <89024189+sujaybn@users.noreply.github.com>
2024-07-08 09:04:38 +0530

    Adding test to check the addition of values with different delimiter

--- src/test/java/org/calc/assesment/StringCalculatorTest.java
+++ src/test/java/org/calc/assesment/StringCalculatorTest.java
@@ -32,6 +32,13 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(6, calculator.add("1\n2,3"));
     }
+
+    @Test
+    public void testDifferentDelimiters() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
     @Test
     public void testNegativeNumbers() {
         StringCalculator calculator = new StringCalculator();
